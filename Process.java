37fa7aa8234da258b7754e0c53871a3712c7daaa
BrandonHayes01 <55750198+BrandonHayes01@users.noreply.github.com>
2019-10-15 15:48:40 +0100

    Add files via upload

---
+++ Process.java
@@ -0,0 +1,11 @@
+
+public class Process {
+
+	public static void main(String[] args) {
+		Student brandon = new Student ("Brandon");
+		System.out.println(brandon);
+		brandon.NameChange("Brandette");
+		System.out.println(brandon);
+	}
+
+}
